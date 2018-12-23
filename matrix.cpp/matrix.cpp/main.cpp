6fc1cbb7551b6d83535c561b038a81fa2de8fb3f
Yuru Tu <707101557@qq.com>
2018-12-23 16:13:09 +0800

    add cpp

---
+++ matrix.cpp/matrix.cpp/main.cpp
@@ -0,0 +1,16 @@
+﻿
+#include "pch.h"
+#include "matrix.h"
+using namespace std;
+
+int main()
+{
+	Matrix A = Matrix(3, 3);
+	cin >> A;
+	Matrix b = Matrix(3, 1);
+	cin >> b;
+	Matrix x = Matrix::Solve(A, b);
+	x.Show();
+	return 0;
+}
+
